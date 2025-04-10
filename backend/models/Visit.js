29fedcb9efc8562482a64f9810f616819e12ff79
Nagasaiprasanna <prasannavemula81@gmail.com>
2025-04-10 17:18:02 +0530

    Visit.js

--- backend/models/Visit.js
+++ backend/models/Visit.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
+
+const visitSchema = new mongoose.Schema({
+  fullName: String,
+  email: String,
+  phoneNumber: String,
+  numPersons: Number,
+  date: String,
+  timeSlot: String,
+  additionalNotes: String,
+  visitType: {
+    type: String,
+    enum: ['virtual', 'in-person'],
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Visit", visitSchema);
 
